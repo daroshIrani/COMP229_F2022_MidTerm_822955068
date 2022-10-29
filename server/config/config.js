4e06ff0ebdd5bc39ef544e1fd3106c8f349b9416
daroshIrani <darosh29@gmail.com>
2022-10-28 20:20:03 -0400

    still trying to debug heroku - created mongo ATlas db conn

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,4 @@
-export const MongoURI = "mongodb://127.0.0.1:27017/media";
+// export const MongoURI = "mongodb://127.0.0.1:27017/media";
 export const Secret = "someSecret";
+
+export const MongoURI = "mongodb+srv://darosh:darosh123@cluster0.rgtxyxa.mongodb.net/?MidetermmediaretryWrites=true&w=majority"
