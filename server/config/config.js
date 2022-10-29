cca93f442450df0d34fcdfafd08d0739463a361d
daroshIrani <darosh29@gmail.com>
2022-10-28 20:31:51 -0400

    debgging

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
 // export const MongoURI = "mongodb://127.0.0.1:27017/media";
 export const Secret = "someSecret";
 
-export const MongoURI = "mongodb+srv://darosh:darosh123@cluster0.rgtxyxa.mongodb.net/?MidetermmediaretryWrites=true&w=majority"
+export const MongoURI = "mongodb+srv://darosh:darosh123@cluster0.rgtxyxa.mongodb.net/Midetermmedia?retryWrites=true&w=majority"
